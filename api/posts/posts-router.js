f193eb72e10d0fe52dd493ddd299856802b83673
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-11 14:40:31 -0600

    prgress on posts-router, server, index, but running into a crashing bug

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const express = require('express');
+const Post = require('./posts-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Post.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: "The posts information could not be retrieved" });
+        })
+})
