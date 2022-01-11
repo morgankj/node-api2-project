33fd8d5114f049f4b249f53b0269389fa8b69874
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-11 16:29:08 -0600

    Completed GET task 6 for comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -92,4 +92,23 @@ router.delete('/:id', (req, res) => {
         })
 })
 
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(foundPost => {
+            if (!foundPost) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else {
+                Post.findPostComments(id)
+                    .then(comments => {
+                        res.status(200).json(comments);
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        res.status(500).json({ message: "The comments information could not be retrieved" })
+                    })
+            }
+        })
+})
+
 module.exports = router;
